8a968b8dd1a367a608e6e1b8c8883aa55210a80f
chung <buitrung200513@gmail.com>
2025-09-05 11:35:11 +0700

    update Hang

---
+++ case_study/src/main/java/com/example/case_study/model/Hang.java
@@ -0,0 +1,30 @@
+package com.example.case_study.model;
+
+public class Hang {
+    private int id;
+    private String tenHang;
+
+    public Hang() {
+    }
+
+    public Hang(int id, String tenHang) {
+        this.id = id;
+        this.tenHang = tenHang;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTenHang() {
+        return tenHang;
+    }
+
+    public void setTenHang(String tenHang) {
+        this.tenHang = tenHang;
+    }
+}
